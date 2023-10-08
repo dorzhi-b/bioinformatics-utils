2bf969a286c6f4f7912f6afc0cbe639c0f211005
dorzhi-b <144492757+dorzhi-b@users.noreply.github.com>
2023-10-09 01:51:17 +0300

    Update dna_rna_tools.py

--- bioinformatics_toolkit/dna_rna_tools.py
+++ bioinformatics_toolkit/dna_rna_tools.py
@@ -1,41 +1,34 @@
-#!/usr/bin/env python
-# coding: utf-8
+from typing import List, Union
 
-# In[175]:
+COMPLEMENT_DICT_DNA = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C', 'a': 't', 't': 'a', 'c': 'g', 'g': 'c'}
+COMPLEMENT_DICT_RNA = {'A': 'U', 'U': 'A', 'C': 'G', 'G': 'C', 'a': 'u', 'u': 'a', 'c': 'g', 'g': 'c'}
+ALPHABET = {'a', 't', 'u', 'g', 'c', 'A', 'T', 'U', 'G', 'C'}
 
-
-def transcribe(seq):
+def transcribe(seq: str) -> Union[str, List[str]]:
     """
     Transcribe a DNA sequence to RNA.
     
-    Parameters
-    ----------
-    seq : str
-        DNA sequence.
-
-    Returns
-    -------
-    str
-        Transcribed RNA sequence.
+    Parameters:
+        seq (str): The DNA sequence to transcribe.
+    
+    Returns:
+        Union[str, List[str]]: The transcribed RNA sequence.
+            If multiple sequences are provided, a list of transcribed sequences is returned.
     """
-    if 'U' and 'u' in seq:
-        return "Can't transcribe RNA."
+    if 'U' in seq or 'u' in seq:
+        return "Invalid sequence. Contains 'U' in DNA sequence."
     
     return seq.replace('T', 'U').replace('t', 'u')
 
-def reverse(seq):
+def reverse(seq: str) -> str:
     """
     Reverse a DNA/RNA sequence.
     
-    Parameters
-    ----------
-    seq : str
-        DNA/RNA sequence.
-
-    Returns
-    -------
-    str
-        Reversed DNA/RNA sequence.
+    Parameters:
+        seq (str): The DNA/RNA sequence to reverse.
+    
+    Returns:
+        str: The reversed DNA/RNA sequence.
     """
     seq_u = seq.upper()
     if 'T' in seq_u and 'U' in seq_u:
@@ -43,86 +36,76 @@ def reverse(seq):
     
     return seq[::-1]
 
-def complement(seq):
+def complement(seq: str) -> str:
     """
-    Find the complement of a DNAsequence.
+    Find the complement of a DNA sequence.
     
-    Parameters
-    ----------
-    seq : str
-        DNA sequence.
-
-    Returns
-    -------
-    str
-        Complement DNA sequence.
+    Parameters:
+        seq (str): The DNA sequence to find the complement for.
+    
+    Returns:
+        str: The complement DNA sequence.
     """
     seq_u = seq.upper()
     if 'T' in seq_u and 'U' in seq_u:
         return "Invalid sequence. Contains both 'T' and 'U'."
     
     if 'T' in seq or "t" in seq:
-        complement_dict_dna = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C', 'a': 't', 't': 'a', 'c': 'g', 'g': 'c'}
-        return ''.join(complement_dict_dna[base] for base in seq)
+        return ''.join(COMPLEMENT_DICT_DNA[base] for base in seq)
     else:
-        complement_dict_rna = {'A': 'U', 'U': 'A', 'C': 'G', 'G': 'C', 'a': 'u', 'u': 'a', 'c': 'g', 'g': 'c'}
-        return ''.join(complement_dict_rna[base] for base in seq)
-    
+        return ''.join(COMPLEMENT_DICT_RNA[base] for base in seq)
 
-def reverse_complement(seq):
+def reverse_complement(seq: str) -> str:
     """
     Find the reverse complement of a DNA sequence.
     
-    Parameters
-    ----------
-    seq : str
-        DNA sequence.
-
-    Returns
-    -------
-    str
-        Compliment reversed DNA sequence.
+    Parameters:
+        seq (str): The DNA sequence to find the reverse complement for.
+    
+    Returns:
+        str: The reverse complement DNA sequence.
     """
     return reverse(complement(seq))
 
-def run_dna_rna_tools(*args, alphabet = {'a', 't', 'u', 'g', 'c', 'A', 'T', 'U', 'G', 'C'}):
+def run_dna_rna_tools(*args: Union[str, List[str]], ALPHABET: set = ALPHABET) -> Union[str, List[str]]:
     """
     Perform the specified operation on the given DNA/RNA sequences.
     
     This function performs the following operations: transcribtion, reversation, complementation, 
     reversal complementation.
     
-    Parameters
-    ----------
-    *args : str
-        DNA/RNA sequence.
-    alphabet : set
-        Default set of valid nucleotide characters: {'a', 't', 'u', 'g', 'c', 'A', 'T', 'U', 'G', 'C'}
+    Parameters:
+        *args (Union[str, List[str]]): DNA/RNA sequence(s).
+        ALPHABET (set): Default set of valid nucleotide characters.
+    
+    Returns:
+        Union[str, List[str]]: The result of the specified operation on the sequence(s).
+            If multiple sequences are provided, a list of results is returned.
 
-    Returns
-    -------
-    str
-        Reversed DNA sequence.
+    Examples:
+        # Transcribing a single DNA sequence
+        run_dna_rna_tools("ATGCTA", "transcribe")
+        # Result: "AUGCUA"
+
+        # Transcribing multiple DNA sequences
+        run_dna_rna_tools("ATGCTA", "TTAGCG", "transcribe")
+        # Result: ["AUGCUA", "UUAGCG"]
     """
-    ops = {
+    operations = {
         'transcribe': transcribe,
         'reverse': reverse,
         'complement': complement,
         'reverse_complement': reverse_complement
     }
     
-    seqs = args[:-1]
-    op = args[-1]
+    seqs, operation = args[:-1], args[-1]
     
-    if all(base in alphabet for seq in seqs for base in seq):
-        if op in ops:
-            results = [ops[op](seq) for seq in seqs]
-            if len(results) == 1:
-                return results[0]
-            else:
-                return results
-        else:
-            return "Invalid operation. Supported operations: transcribe, reverse, complement, reverse_complement"
+    if  not all(base in ALPHABET for seq in seqs for base in seq):
+        return "Not DNA/RNA. Use only standard nucleotide sequences."
+    if not operation in operations:
+        return "Invalid operation. Supported operations: transcribe, reverse, complement, reverse_complement."
+    results = [operations[operation](seq) for seq in seqs]
+    if len(results) == 1:
+        return results[0]
     else:
-        return "Not DNA/RNA. Use only standard nucleotide sequences"
-
+        return results
