087d5136d074a945491316a4799dccc618dda4bf
dorzhi-b <144492757+dorzhi-b@users.noreply.github.com>
2023-10-19 00:53:07 +0300

    Add change_fasta_start_pos function

--- bio_files_processor.py
+++ bio_files_processor.py
@@ -29,3 +29,34 @@ def convert_multiline_fasta_to_oneline(input_fasta: str, output_fasta: str = Non
                 current_sequence += line.strip()
         if current_sequence:
             output_file.write(current_sequence + '\n')
+
+def change_fasta_start_pos(input_fasta: str, shift: int, output_fasta: str = None):
+    """
+    Shifts the start position of the sequence in a FASTA file by a specified number of positions.
+
+    Parameters:
+    -----------
+    input_fasta (str):
+        The path to the input FASTA file.
+
+    shift (int):
+        The number of positions by which the start position of the sequence should be shifted. 
+        A positive value shifts to the right, and a negative value shifts to the left.
+
+    output_fasta (str, optional):
+        The path to the output FASTA file. If not provided, the default output filename will be
+        created based on the input filename by replacing '.fasta' with '_.fasta'.
+    """
+    
+    if output_fasta is None:
+        output_fasta = input_fasta.replace('.fasta', '_.fasta')
+
+    with open(input_fasta, 'r') as f:
+        data = f.read().strip().split('\n')
+        header = data[0]
+        sequence = data[1]
+
+    shifted_sequence = sequence[shift:] + sequence[:shift]
+
+    with open(output_fasta, 'w') as f:
+        f.write(f"{header}\n{shifted_sequence}\n")
